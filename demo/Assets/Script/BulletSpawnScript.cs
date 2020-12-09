d5a74eda135c86e18e0ef91d5d4588b31bdda1f8
Kim Young June <rladudwns4643@gmail.com>
2020-12-09 14:45:23 +0900

    demo(unity)

---
+++ demo/Assets/Script/BulletSpawnScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawnScript : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform bulletpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Instantiate(Bullet, bulletpos.transform.position, bulletpos.transform.rotation);
+        }
+    }
+}
