4102a8bd4731c6299b6e20906f28439b0cfc4fad
Kime Dong Seok <DorongSeok@daum.net>
2021-02-02 15:51:32 +0900

    모델 적용 연습용 프로젝트 추가

---
+++ 모델적용 연습/LabProject/Scene.cpp
@@ -0,0 +1,144 @@
+#include "Scene.h"
+
+CScene::CScene()
+{
+	// m_pd3dPipelineState = NULL;
+	m_pd3dGraphicsRootSignature = NULL;
+}
+
+void CScene::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
+{
+	// 그래픽 루트 시그너쳐를 생성한다.
+	m_pd3dGraphicsRootSignature = CreateGraphicsRootSignature(pd3dDevice);
+
+	// 가로x세로x깊이가 12x12x12인 정육면체 메쉬를 생성한다.
+	CCubeMeshDiffused* pCubeMesh = new CCubeMeshDiffused(pd3dDevice, pd3dCommandList, 12.0f, 12.0f, 12.0f);
+
+	m_nObjects = 1;
+	m_ppObjects = new CGameObject * [m_nObjects];
+	CRotatingObject* pRotatingObject = new CRotatingObject();
+
+	pRotatingObject->SetMesh(pCubeMesh);
+	CDiffusedShader* pShader = new CDiffusedShader();
+
+	pShader->CreateShader(pd3dDevice, m_pd3dGraphicsRootSignature);
+	pShader->CreateShaderVariables(pd3dDevice, pd3dCommandList);
+
+	pRotatingObject->SetShader(pShader);
+
+	m_ppObjects[0] = pRotatingObject;
+}
+
+void CScene::ReleaseObjects()
+{
+	if (m_pd3dGraphicsRootSignature) 
+		m_pd3dGraphicsRootSignature->Release();
+
+	if (m_ppObjects)
+	{
+		for (int j = 0; j < m_nObjects; j++)
+			if (m_ppObjects[j])
+				delete m_ppObjects[j];
+		delete[] m_ppObjects;
+	}
+}
+
+void CScene::ReleaseUploadBuffers()
+{
+	if (m_ppObjects)
+	{
+		for (int j = 0; j < m_nObjects; j++)
+			if (m_ppObjects[j])
+				m_ppObjects[j]->ReleaseUploadBuffers();
+	}
+}
+
+ID3D12RootSignature* CScene::CreateGraphicsRootSignature(ID3D12Device* pd3dDevice)
+{
+	ID3D12RootSignature* pd3dGraphicsRootSignature = NULL;
+
+	D3D12_ROOT_PARAMETER pd3dRootParameters[2];
+	pd3dRootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+	pd3dRootParameters[0].Constants.Num32BitValues = 16;
+	pd3dRootParameters[0].Constants.ShaderRegister = 0;
+	pd3dRootParameters[0].Constants.RegisterSpace = 0;
+	pd3dRootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+	pd3dRootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+	pd3dRootParameters[1].Constants.Num32BitValues = 32;
+	pd3dRootParameters[1].Constants.ShaderRegister = 1;
+	pd3dRootParameters[1].Constants.RegisterSpace = 0;
+	pd3dRootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+
+	D3D12_ROOT_SIGNATURE_FLAGS d3dRootSignatureFlags =
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS |
+		D3D12_ROOT_SIGNATURE_FLAG_DENY_PIXEL_SHADER_ROOT_ACCESS;
+	D3D12_ROOT_SIGNATURE_DESC d3dRootSignatureDesc;
+	::ZeroMemory(&d3dRootSignatureDesc, sizeof(D3D12_ROOT_SIGNATURE_DESC));
+	d3dRootSignatureDesc.NumParameters = _countof(pd3dRootParameters);
+	d3dRootSignatureDesc.pParameters = pd3dRootParameters;
+	d3dRootSignatureDesc.NumStaticSamplers = 0;
+	d3dRootSignatureDesc.pStaticSamplers = NULL;
+	d3dRootSignatureDesc.Flags = d3dRootSignatureFlags;
+
+	ID3DBlob* pd3dSignatureBlob = NULL;
+	ID3DBlob* pd3dErrorBlob = NULL;
+	::D3D12SerializeRootSignature(&d3dRootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &pd3dSignatureBlob, &pd3dErrorBlob);
+	pd3dDevice->CreateRootSignature(0, pd3dSignatureBlob->GetBufferPointer(), pd3dSignatureBlob->GetBufferSize(), __uuidof(ID3D12RootSignature), (void**)&pd3dGraphicsRootSignature);
+	if (pd3dSignatureBlob) pd3dSignatureBlob->Release();
+	if (pd3dErrorBlob) pd3dErrorBlob->Release();
+
+	return(pd3dGraphicsRootSignature);
+}
+
+
+ID3D12RootSignature* CScene::GetGraphicsRootSignature()
+{
+	return(m_pd3dGraphicsRootSignature);
+}
+
+// 마우스
+bool CScene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+	lParam)
+{
+	return(false);
+}
+// 키보드
+bool CScene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam,
+	LPARAM lParam)
+{
+	return(false);
+}
+
+bool CScene::ProcessInput(UCHAR* pKeysBuffer)
+{
+	return(false);
+}
+
+// 애니메이션
+void CScene::AnimateObjects(float fTimeElapsed)
+{
+	for (int j = 0; j < m_nObjects; j++)
+	{
+		m_ppObjects[j]->Animate(fTimeElapsed);
+	}
+}
+
+// 그리기
+void CScene::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera)
+{
+	pCamera->SetViewportsAndScissorRects(pd3dCommandList);
+
+	// 그래픽 루트 시그너쳐를 파이프라인에 연결(설정)한다.
+	pd3dCommandList->SetGraphicsRootSignature(m_pd3dGraphicsRootSignature);
+
+	if (pCamera) pCamera->UpdateShaderVariables(pd3dCommandList);
+
+	// 씬을 렌더링하는 것은 씬을 구성하는 게임 객체(셰이더를 포함하는 객체)들을 렌더링하는 것이다.
+	for (int j = 0; j < m_nObjects; j++)
+	{
+		if (m_ppObjects[j]) m_ppObjects[j]->Render(pd3dCommandList, pCamera);
+	}
+}
---
+++ 모델적용 연습/LabProject/Timer.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "stdafx.h"
+
+const ULONG MAX_SAMPLE_COUNT = 50; // 50회의 프레임 처리시간을 누적하여 평균한다.
+class CGameTimer
+{
+public:
+	CGameTimer();
+	virtual ~CGameTimer();
+
+	// 타이머의 시간 갱신
+	void Tick(float fLockFPS = 0.0f);
+
+	// 프레임 레이트 반환
+	unsigned long GetFrameRate(LPTSTR lpszString = NULL, int nCharacters = 0);
+
+	// 프레임 평균 경과 시간 반환
+	float GetTimeElapsed();
+
+private:
+	// 컴퓨터가 PerformanceCounter를 가지고 있는가
+	bool m_bHardwareHasPerformanceCounter;
+	// Scale Counter의 양
+	float m_fTimeScale;
+	// 마지막 프레임 이후 지나간 시간
+	float m_fTimeElapsed;
+	// 현재의 시간
+	__int64 m_nCurrentTime;
+	// 마지막 프레임의 시간
+	__int64 m_nLastTime;
+	// 컴퓨터의 Performance Frequency
+	__int64 m_nPerformanceFrequency;
+
+	// 프레임 시간을 누적하기 위한 배열
+	float m_fFrameTime[MAX_SAMPLE_COUNT];
+	// 누적된 프레임 횟수
+	ULONG m_nSampleCount;
+
+	// 현재의 프레임 레이트
+	unsigned long m_nCurrentFrameRate;
+	// 초당 프레임 수
+	unsigned long m_nFramesPerSecond;
+	// 프레임 레이트 계산 소요시간
+	float m_fFPSTimeElapsed;
+};
