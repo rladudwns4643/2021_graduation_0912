8eceecad29af17ce4077c0a52d68b74ed0ac80b8
Kim Young June <rladudwns4643@gmail.com>
2021-05-07 01:03:12 +0900

    demo patch
    어차피 다 auto인데 auto_accept 키워드가 어색함

--- demo_server/demo_server/protocol.h
+++ demo_server/demo_server/protocol.h
@@ -1,5 +1,5 @@
 #pragma once
-//#include "../../demo_battle/demo_battle/MathHelper.h"
+#include "../../demo_battle/demo_battle/MathHelper.h"
 //------------------------------------------------------------------
 #define LOBBY_SERVER_PORT	7237
 #define BATTLE_SERVER_PORT	7238
@@ -13,8 +13,7 @@
 //------------------------------------------------------------------
 
 //------------------------------------------------------------------
-#define MAX_ID_LEN		50
-#define MAX_STR_LEN		50
+#define MAX_ID_LEN		10
 //------------------------------------------------------------------
 
 
@@ -53,9 +52,8 @@
 //------------------------------------------------------------------
 
 //------------------------------------------------------------------
-#define MAX_ROOM_COUNT 5
+#define MAX_ROOM_COUNT 1
 //------------------------------------------------------------------
-
 
 //-------------------------------L->C-------------------------------
 //6
@@ -64,7 +62,7 @@
 #define LC_SIGNUP_OK				32
 #define LC_SIGNUP_FAIL				33
 #define LC_USERINFO					34
-#define LC_CANCEL_AUTOMATCH_SUCCESS 35
+#define LC_CANCEL_MATCH_OK			35
 #define LC_MATCHSTART				36
 
 #pragma pack(push, 1)
@@ -98,7 +96,7 @@ struct lc_pakcet_signup_fail {
 	BYTE type;
 };
 
-struct lc_packet_cancel_automatch_success {
+struct lc_packet_cancel_match_ok {
 	BYTE size;
 	BYTE type;
 };
@@ -125,11 +123,12 @@ struct lc_packet_startMatch {
 #define CL_CANCEL_AUTOMATCH			25
 
 #pragma pack(push, 1)
-struct cl_pakcet_login {
+struct cl_packet_login {
 	BYTE size;
 	BYTE type;
 
 	char id[MAX_ID_LEN];
+	char pw[MAX_ID_LEN];
 };
 
 struct cl_pakcet_signup {
@@ -173,8 +172,8 @@ struct cl_packet_cancel_automatch {
 
 //-------------------------------B->C-------------------------------
 //22 + (bb, START_INFO, VECTOR, ROOM)
-#define BC_AUTO_ACCEPT_OK		1
-#define BC_AUTO_ACCEPT_FAIL		2
+#define BC_ACCEPT_OK			1
+#define BC_ACCEPT_FAIL			2
 #define BC_JOIN_OK				3
 #define BC_JOIN_FAIL			4
 
@@ -294,7 +293,8 @@ struct bc_packet_game_start {
 	BYTE size;
 	BYTE type;
 
-	PTC_START_INFO start_info;
+	PTC_START_INFO start_info[2];
+	char map_type;	//unuse 210506(map 추가시 추가)
 };
 
 struct bc_packet_gamestart_available {
