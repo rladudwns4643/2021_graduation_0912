488074195f3cc1b18614f0d20fc796eddb4c2b2a
Kime Dong Seok <DorongSeok@daum.net>
2021-05-09 18:51:41 +0900

    Revert "Revert "Revert "삭제"""
    This reverts commit f2163aab3ec9c18ed3b50fc203934ec625a68879.

---
+++ ToyGround/LabProject/GameObject.h
@@ -0,0 +1,88 @@
+#pragma once
+
+class SkinnedModelInstance;
+class GeometryMesh;
+class GameObject
+{
+protected:
+	/* 중요 멤버변수 키워드*/
+	// m_MeshName: 메시이름, 클라이언트에서 객체를 찾을때 필요한 키값이기도 함
+	// m_InstID: 인스턴스 객체의 고유아이디, ObjectInfo의 인스턴스 객체를 관리하는 맵에서 객체의 인덱스를 찾을때 필요 (prop: servermeshID를 string으로 변형한값 / ui: uiName)
+	// m_Index: ApplicationContext에 모든 객체의 정보를 담고있는 m_RItemsVec의 인덱스 정보
+	// m_ServerMeshID: 서버에서 정의한 키값, typedef의 메시이름과 타입아이디가 정의되어있다. 
+		// 클라이언트는 meshname(string)로 키값을 사용하고, 서버는 typeID(int)를 키값으로 사용한다.
+		// 패킷으로는 typeID(int)값이 온다.
+
+	std::string m_MeshName;
+	std::string m_InstID;
+	UINT		m_Index;
+
+public:
+	explicit GameObject(std::string meshName, std::string instID);
+	virtual ~GameObject();
+
+	std::string GetMeshName() const { return m_MeshName; }
+	std::string	GetInstID() const { return m_InstID; }
+	UINT		GetIndex() const { return m_Index; }
+
+	virtual void InitializeTransform();
+	virtual void Update(const float deltaT);
+	virtual bool SetMesh(std::string meshName, std::string submeshName);
+
+public:
+	virtual void SetPosition(float posX, float posY, float posZ);
+	virtual void SetPosition(DirectX::XMFLOAT3 xmPos);
+
+	virtual void SetRight(const DirectX::XMFLOAT3& Right);
+	virtual void SetUp(const DirectX::XMFLOAT3& Up);
+	virtual void SetLook(const DirectX::XMFLOAT3& Look);
+	virtual void SetMatrixByLook(float x, float y, float z);
+
+	XMFLOAT3 GetPosition() const;
+	XMFLOAT3 GetLook() const;
+	XMFLOAT3 GetUp() const;
+	XMFLOAT3 GetRight() const;
+
+	virtual void MoveStrafe(float fDistance = 1.0f);
+	virtual void MoveUp(float fDistance = 1.0f);
+	virtual void MoveForward(float fDistance = 1.0f);
+
+	virtual void Move(DWORD dwDirection, float fDistance, bool bUpdateVelocity);
+	virtual void Move(const XMFLOAT3& xmf3Shift, bool bVelocity = false);
+
+	virtual void Rotate(const DirectX::XMFLOAT3& axis, float angle);
+	virtual void Rotate(const DirectX::XMFLOAT4& quaternion);
+	virtual void Rotate(float pitch, float yaw, float roll);
+
+	virtual void Scale(float x, float y, float z);
+
+public:
+	// 기타 특성값
+	bool m_IsCulling = false;
+
+	int m_ZLayer = -1;
+
+	// 월드좌표 및 텍스쳐좌표
+	DirectX::XMFLOAT4X4 m_World;
+	DirectX::XMFLOAT4X4 m_TexTransform;
+	XMFLOAT2 m_PositionRatio = { 0.f, 0.f }; // 화면비율
+	XMFLOAT2 m_SizeRatio = { 0.f, 0.f }; // 화면비율
+
+
+	// 메쉬, 바운딩박스정보
+	GeometryMesh* m_Geo;
+	D3D12_PRIMITIVE_TOPOLOGY m_PrimitiveType;
+	UINT m_IndexCount;
+	UINT m_StartIndexLocation;
+	int m_BaseVertexLocation;
+	BoundingBox m_Bounds;
+
+	// 재질정보
+	UINT m_MaterialIndex;
+	UINT m_OriginMatIndex;
+
+	// 애니메이션 정보
+	SkinnedModelInstance* m_SkinnedModelInst = NULL;
+	UINT m_SkinnedCBIndex = -1;
+};
+
---
+++ ToyGround/LabProject/framework.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <winsock.h> // 필요 없을듯
+#include <WinSock2.h>
+#include <WindowsX.h>
+#include <Windows.h>
+#include <shellapi.h>
+#include <wrl.h>
+#include <Mmsystem.h>
+
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32")
+
+// For Play Video 
+#include <Vfw.h>
+#pragma comment(lib, "vfw32.lib")
+
+
+// C 런타임 헤더 파일입니다.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <fstream>
+#include <comdef.h>
+#include <float.h>
+#include <cstdint>
+
+// C++ 런타임 헤더 파일입니다.
+#include <iostream>
+#include <array>
+#include <list>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <iterator>
+#include <assert.h>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <sstream>
+
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+// DirectX 런타임 헤더 파일입니다.
+#include "d3dx12.h"
+//#include <d3d11_1.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+// D2D (for Render Text)
+#include <d2d1_3.h>
+#include <dwrite.h>
+#include <d3d11on12.h>
+
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+#pragma comment(lib, "d3d11.lib")
+
+//network
+#pragma comment (lib, "ws2_32.lib")
+
+
+using namespace std;
+//using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+using namespace DirectX::PackedVector;
+
+#define AppContext ApplicationContext::GetApp()
+
+#define SAFE_DELETE_PTR(ptr)	\
+{								\
+	if (ptr != nullptr)			\
+	{							\
+		delete ptr;				\
+		ptr = nullptr;			\
+	}							\
+}
