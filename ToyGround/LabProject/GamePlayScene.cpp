cc9190b8b838a7295d001635c383aababe51d67d
Kim Young June <rladudwns4643@gmail.com>
2021-08-15 00:33:56 +0900

    init animation idle mode

--- ToyGround/LabProject/GamePlayScene.cpp
+++ ToyGround/LabProject/GamePlayScene.cpp
@@ -45,8 +45,14 @@ void GameplayScene::ProcessEvent(int sEvent, int argsCount, ...) {
 		m_Users[arg_bt_id]->m_MapName = m_MapName;
 		m_Users[arg_bt_id]->m_PlayerID = arg_bt_id;
 		//m_Users[arg_bt_id]->m_SpawnLoaction = arg_sl;
-		CommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[1]);
-		EnemyCommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[2]);
+		if (Service::GetApp()->GetMyBattleID() == 1) {
+			CommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[1]);
+			EnemyCommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[2]);
+		}
+		else {
+			CommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[2]);
+			EnemyCommandCenter::GetApp()->PushCommand<MoveCommand>(static_cast<int>(MoveState::Idle), m_Users[1]);
+		}
 		break;
 	}
 	case EVENT_GAME_ROUND_START: {
